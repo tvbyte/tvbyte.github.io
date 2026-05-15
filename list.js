44e015963cffa69cc529403ad0db174c76feaff5
tvbyte <sk01677603434@gmail.com>
2026-05-16 01:05:53 +0600

    Update list.js

--- list.js
+++ list.js
@@ -1,4 +1,6 @@
 const linksData = [
+{ id: 8, name: 'Mxonlive', url: '/tvsite/mxonlive/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+{ id: 7, name: 'Idhamaka', url: ' https://idhamaka.vercel.app/ ',  labels: ['Bdix', 'Link'], category: 'global', updated: true, tech: 'M3U', access: 'link', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
 { id: 6, name: 'Fanzone', url: '/tvsite/fanzone/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
 { id: 5, name: 'Mastertv', url: '/tvsite/mastertv/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
 { id: 4, name: 'Kbtv', url: '/tvsite/tplay/kbtv.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'JS', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },    
