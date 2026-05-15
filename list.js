a0ba9a2bb4b6b241287bcdd00ab3bc2145bf2692
tvbyte <sk01677603434@gmail.com>
2026-05-15 22:50:16 +0600

    Add files via upload

---
+++ list.js
@@ -0,0 +1,5 @@
+    const linksData = [
+      { id: 3, name: 'Biostartv', url: '/tvsite/biostartv/index.html', labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+      { id: 2, name: 'Fanbaetv', url: '/tvsite/banbaetv/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+      { id: 1, name: 'Tplay', url: '/tvsite/tplay/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'JS', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+    ];
