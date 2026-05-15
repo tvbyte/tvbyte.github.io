1429569e1c1e222b1b905f8c6eaa24f21bbcbfbc
tvbyte <sk01677603434@gmail.com>
2026-05-15 23:47:01 +0600

    Update list.js

--- list.js
+++ list.js
@@ -1,5 +1,8 @@
-    const linksData = [
-      { id: 3, name: 'Biostartv', url: '/tvsite/biostartv/index.html', labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
-      { id: 2, name: 'Fanbaetv', url: '/tvsite/fanbaetv/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
-      { id: 1, name: 'Tplay', url: '/tvsite/tplay/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'JS', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+const linksData = [
+{ id: 6, name: 'Fanzone', url: '/tvsite/fanzone/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+{ id: 5, name: 'Mastertv', url: '/tvsite/mastertv/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+{ id: 4, name: 'Kbtv', url: '/tvsite/tplay/kbtv.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'JS', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },    
+{ id: 3, name: 'Biostartv', url: '/tvsite/biostartv/index.html', labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+{ id: 2, name: 'Fanbaetv', url: '/tvsite/fanbaetv/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'M3U', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
+{ id: 1, name: 'Tplay', url: '/tvsite/tplay/index.html',  labels: ['Bdix', 'Host'], category: 'global', updated: true, tech: 'JS', access: 'host', disabled: false, description: 'Live TV  from your favorite channels on any device.' },
     ];
